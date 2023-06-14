1edb750297c3c2d1fa81cd73876bfb4dc7e34374
Mari <Mari_Hanayo_Mayuri@gitlab.com>
2023-06-14 09:52:10 +0200

    added cleaning and variant extraction to class VariantsFinder

--- pkg/Extraction.py
+++ pkg/Extraction.py
@@ -1,16 +1,20 @@
  
 import nltk
-nltk.download('punkt')
 from itertools import combinations
 from bs4 import BeautifulSoup
 import re
 from collatex import Collation, collate
+import unicodedata
+
+
+nltk.download('punkt')
 
 ################################################################ CLASS DEFINITIONS
 
 class VariantsFinder:
     """
     This class create object handling the variants analysis between a list of text in ancien hebrew
+    Class made by Prunelle
     """
 
     def __init__(self, fileNames:list):
@@ -18,11 +22,21 @@ class VariantsFinder:
         Create an object of type VariantsFinder
         """
         self.fileNames = fileNames
-        self.witnesses, self.chap_info = verseMatching(fileNames)
+        self.witnesses, self.chap_info = verse_matching(fileNames)
+        print("# Matching Done")
+
+        for witness in self.witnesses.values():
+            for verse in witness.values():
+                verse = clean_hebrew_punctuations(verse)
+        print("## Cleaning Done")
+
+        self.variants = verse_collation(self.witnesses)
+        print("### Collation Done")
 
     def getSampleVariant(self, x:int = 1):
         """
         Gives the variant of a sample verse, by default 1
+        Function made by Shehenaz, adapted into method by Prunelle
         """
         collation = Collation()
         collation.add_plain_witness("A", self.witnesses[x]['A'])
@@ -40,14 +54,18 @@ def test():
     """
     print("test")
 
-def verseMatching(fileNames):
+def verse_matching(fileNames):
+    """
+    NEED DOCSTRING
+    Algorithm made by Shehnaz, adapted into function by Prunelle
+    """
     soup = dict()
     chapter = dict()
     verse_list = dict()
     verses = dict()
 
     for currFile in fileNames:
-        with open("WorkDir/"+currFile,encoding="utf8") as fp:
+        with open("WorkDir/"+currFile,encoding="utf8") as fp: #TODO : get rid of the workdir stuff
             soup[currFile] = BeautifulSoup(fp,features='xml')
             chs = list()
             verse_list[currFile] = dict()
@@ -119,3 +137,52 @@ def verseMatching(fileNames):
         chap_matching[combo[0]][combo[1]]=matching_chs_list
         
         return witnesses, chap_info
+    
+def clean_hebrew_punctuations(text):
+    """
+    NEED DOCSTRING
+    Function made by Shehnaz
+    """
+    filtered_text = ''
+    for char in text:
+        if '\u0591' <= char <= '\u05F4' or '\uFB1D' <= char <= '\uFB4F' or char == '\u0020' or char =='\u0009':
+            if char != '\u059F':  # Exclude this character
+                if char == '\u0009':
+                    filtered_text += '\u0020'
+                else:
+                    filtered_text += char
+    return filtered_text
+
+def verse_collation(witnesses):
+    """
+    NEED DOCSTRING
+    Algorithm made by Shehnaz, adapted into function by Prunelle
+    """
+    # Use the collatex library to perform the collation and build the variants
+
+    num_witnesses = len(witnesses)
+    # Lets create an empty dictionary to store the variants
+    variants = dict()
+    # For chapter information we can refer to the dictionary chap_info
+    # The dictionary indexes are same across all the data
+
+    # Lets loop of each witness
+    for witness_ind in range(num_witnesses):
+        # Lets run the collation for each set of witnesses
+        # Create empty dictionary for each element
+        variants[witness_ind] = dict()
+        variants[witness_ind]['A'] = '' # Empty initialisatiom
+        variants[witness_ind]['B'] = ''
+        collation = Collation()
+        collation.add_plain_witness("A", witnesses[witness_ind]['A'])
+        collation.add_plain_witness("B", witnesses[witness_ind]['B'])
+        # Perform the collation
+        alignment_table = collate(collation)
+        # Lets start trying to find the variants
+        for column in alignment_table.columns:
+            if column.variant:
+            # Add to variants the tokens as strings       
+                for manuscript, tokens in column.tokens_per_witness.items():
+                    token_strings = [token.token_string for token in tokens]
+                    variants[witness_ind][manuscript] += ' '.join(token_strings)
+    return variants
